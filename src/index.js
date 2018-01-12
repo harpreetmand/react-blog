6cfbbed2a15cda9b9bdc117d66e2bd4395870b21
Harpreet <harpreetmand@gmail.com>
2018-01-12 11:14:10 -0500

    refactor and added router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './containers/App/App';
-import AnotherApp from './containers/App/AnotherApp';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom'
 
-ReactDOM.render(
-    <div>
-<App />
-<AnotherApp />
-</div>
-, document.getElementById('root'));
-registerServiceWorker();
+import './index.scss'
+import App from './components/App';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+    render() { return <div> Hello!</div> }
+}
+class Goodbye extends React.Component {
+    render() { return <div> Goodbye!</div> }
+}
+
+ReactDom.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <App />
+                <Route path="/hello" component={Hello} />
+                <Route path="/goodbye" component={Goodbye} />
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
